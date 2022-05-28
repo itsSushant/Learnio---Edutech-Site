7fa8517b57de2ea0ec9cd33b2d8d837775ba35ad
Sushant Jadhav <64315421+itsSushant@users.noreply.github.com>
2022-05-28 11:37:38 +0530

    Add files via upload

---
+++ LEARNIO - Site/script.js
@@ -0,0 +1,27 @@
+let hamMenuIcon = document.getElementById("ham-menu");
+let navBar = document.getElementById("nav-bar");
+let navLinks = navBar.querySelectorAll("li");
+let scrollTopBtn = document.getElementById("scroll-top");
+
+
+
+let header = document.querySelector("header");
+window.onscroll = () => {
+  /* Sticky Header */
+  let pos = document.documentElement.scrollTop;
+  if (pos > 0) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+  /* Scroll Top Button */
+  if (pos > 300) {
+    scrollTopBtn.style.display = "grid";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+
+  scrollTopBtn.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+};
